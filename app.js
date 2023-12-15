9924cb94e672ff5089f53194a2e7e4cf5bed0542
Nicholas Murray <nmurr13@protonmail.com>
2023-12-15 15:37:47 -0500

    main functionality almost complete

--- app.js
+++ app.js
@@ -1,4 +1,33 @@
-const url = "https://cdn.contentful.com/spaces/f9ludc6g4f0p/environments/master/entries/1TH00D0RqYW0BoSmfJusZP?access_token=zvuk1r8PGmzjJxJn6UqY-NhSv2uaYbM4aPj46yiJGQI"
-fetch(url)
-.then((res) => res.json())
-.then((data) => console.log(data))
+let currentQuestionIndex = 0;
+let questionObjects = [];
+fetch('https://cdn.contentful.com/spaces/f9ludc6g4f0p/environments/master/entries?access_token=eXdjQV-YwAoRMbiHd5NIvIqCZPL_taOIx8xqPve9XMk&hockeyTriviaGame')
+.then(response => response.json())
+.then(data => {
+	questionObjects = data.items.map(item => item.fields);
+    populateQuestion(questionObjects[0]);
+})
+.catch(error => console.error('Error:', error));
+
+function populateQuestion(questionObject) {
+	const questionElement = document.querySelector(".question-title");
+	questionElement.textContent = questionObject.questions;
+
+	const answerOneElement = document.querySelector(".answer1");
+	answerOneElement.textContent = questionObject.correctAnswers;
+	
+	const answerTwoElement = document.querySelector(".answer2");
+	answerTwoElement.textContent = questionObject.wrongAnswers;
+
+    const nextQuestionButton = document.getElementById('nextQuestion');
+    nextQuestionButton.onclick = () => {
+        currentQuestionIndex++;
+        if (currentQuestionIndex < questionObjects.length) {
+            populateQuestion(questionObjects[currentQuestionIndex]);
+            } else {
+            currentQuestionIndex = 0;
+            populateQuestion(questionObjects[currentQuestionIndex]);
+            }
+        populateQuestion(questionObjects[currentQuestionIndex]);
+        console.log(currentQuestionIndex);
+         };
+}
